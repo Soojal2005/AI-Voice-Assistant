8c847243ce896bd634d3f2b2cfac2a52b94d127f
Soojal2005 <bajajsoojal123@gmail.com>
2025-05-17 00:21:35 -0400

    first commit

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ import api
 recognizer = sr.Recognizer()
 
 engine = pyttsx3.init()
-# newsapi = "e342c08c9f8a409a91b43fdd0f92bc19"
-# genai_api = "AIzaSyDa4uPEoukYxJYIE7YxHCTlFnuqGdzWNHY"
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
